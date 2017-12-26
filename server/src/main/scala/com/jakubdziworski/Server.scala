ac1ebd043c00f04a62d0af0458bcb988ebc6f212
Salvador Guzman <guzmansalv@gmail.com>
2017-12-26 12:32:59 -0800

    add types to public vals

--- server/src/main/scala/com/jakubdziworski/Server.scala
+++ server/src/main/scala/com/jakubdziworski/Server.scala
@@ -8,22 +8,23 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.stream.ActorMaterializer
 import com.jakubdziworski.service.GameService
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
 
+import scala.concurrent.ExecutionContextExecutor
 import scala.io.StdIn
 
 object Server {
 
   object Config {
-    val config = ConfigFactory.defaultApplication()
-    val port = config.getInt("server.port")
-    val host = config.getString("server.host")
+    val config: Config = ConfigFactory.defaultApplication()
+    val port: Int = config.getInt("server.port")
+    val host: String = config.getString("server.host")
   }
 
   def main(args: Array[String]) {
-    implicit val system = ActorSystem()
-    implicit val materializer = ActorMaterializer()
-    implicit val executionContext = system.dispatcher
+    implicit val system: ActorSystem = ActorSystem()
+    implicit val materializer: ActorMaterializer = ActorMaterializer()
+    implicit val executionContext: ExecutionContextExecutor = system.dispatcher
 
     val gameService = new GameService()
     val bindingFuture = Http().bindAndHandle(gameService.websocketRoute, Config.host,Config.port)
